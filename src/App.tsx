4c9eb980b52975c2d3d0f491314959ca14cd0dca
kamilowski1997 <kamilowski1997@gmail.com>
2023-03-18 16:53:10 +0100

    clear new project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = (): JSX.Element => {
+  return <Text>Hello</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
